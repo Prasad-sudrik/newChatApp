9e84bf4b00a25459f20d63c2b07fff3f9cf12601
Prasad Sudrik <sudrikprasad90@gmail.com>
2023-09-29 19:21:38 +0530

    first commit

---
+++ app/src/app.js
@@ -0,0 +1,27 @@
+// const socket = new Server("ws://localhost:3000");
+const socket = io("ws://localhost:3500");
+
+function sendMessage(e) {
+  e.preventDefault();
+  const input = document.querySelector("input");
+  if (input.value) {
+    socket.emit("message", input.value);
+    input.value = "";
+  }
+  input.focus();
+}
+//
+document.querySelector("form").addEventListener("submit", sendMessage);
+
+// Listen for messages
+// socket.addEventListener("message", ({ data }) => {
+//   const li = document.createElement("li");
+//   li.textContent = data;
+//   document.querySelector("ul").appendChild(li);
+// });
+
+socket.on("message", (data) => {
+  const li = document.createElement("li");
+  li.textContent = data;
+  document.querySelector("ul").appendChild(li);
+});
